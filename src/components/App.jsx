42607ca89656cd68758d6fd3f8d18ff650589768
anny <test@test123.com>
2017-12-06 17:42:34 +0200

    add redux to application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
+import  {firebaseApp} from "../firebase";
+import {connect} from 'react-redux'
+
 
 
 class App extends Component{
+    singOut(){
+        firebaseApp.auth().signOut();
+    }
     render(){
         return (
-            <div>App</div>
+            <div>
+                <h3>Goals</h3>
+                <div>Add Goal</div>
+                <button
+                    className="btn btn-danger"
+                    onClick={()=>this.singOut()}
+                >
+                    Sign Out
+                </button>
+            </div>
         );
     }
 }
+function mapStateToProps(state){
+    console.log('state',state);
+    return {}
+}
 
-export default App
+export default connect(mapStateToProps, null)(App);
--- src/index.js
+++ src/index.js
@@ -3,25 +3,36 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-import { BrowserRouter,Route,Switch } from 'react-router-dom';
+import { BrowserRouter,Route,Switch,Redirect } from 'react-router-dom';
 import {firebaseApp} from "./firebase";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './reducers';
+import {logUser} from "./actions"
 
+const store = createStore(reducer);
 firebaseApp.auth().onAuthStateChanged(
     user=>{
         if(user){
             console.log('user has signed in or up', user);
+
+            const {email} = user;
+            store.dispatch(logUser(email));
+
         }else{
             console.log('user has signed uot or still needs to sign in');
         }
     }
 )
 ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
         <Switch>
         <Route exact path='/' component={App}/>
         <Route path='/signin' component={SignIn}/>
         <Route path='/signup' component={SignUp}/>
         </Switch>
-    </BrowserRouter>,
+    </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 )
