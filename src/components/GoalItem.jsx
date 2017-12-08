6e70633addbdf1fd5083c49ffb924ad6625586ae
anny <test@test123.com>
2017-12-08 11:00:50 +0200

    Complete goal

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
+import {completedGoalRef} from "../firebase";
+import {connect} from 'react-redux';
 
 class GoalItem extends Component{
+    completeGoal(){
+        const {email} = this.props.user;
+        const {title} = this.props.goal;
+        console.log('email',email,'title',title);
+        completedGoalRef.push({email,title});
+    }
     render(){
         const {email,title}=this.props.goal;
         return (
             <div style={{margin:'5px'}}>
                 <strong>{title}</strong>
-                <span> submitted by <em>{email}</em></span>
+                <span style={{margin:'5px'}}> submitted by <em>{email}</em></span>
+                <button
+                    onClick={()=>this.completeGoal()}
+                className="btn btn-sm btn-primary"
+                >
+                    Complete
+                </button>
             </div>
 
         )
     }
 }
+function mapStateToProps(state){
+    const {user}=state;
+    return {user};
+}
 
-export default GoalItem;
+export default connect(mapStateToProps,null)(GoalItem);
